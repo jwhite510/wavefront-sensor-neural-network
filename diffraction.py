319f3d27f16a7028f89e3ee0d982618e0e9d2433
jonathon <jonathonwhite@protonmai.com>
2019-11-27 11:43:47 +0100

    simulating fraunhofer diffraction

--- diffraction.py
+++ diffraction.py
@@ -59,7 +59,52 @@ def fresnel_diffraction():
 
 
 
+
+def create_pattern(Nx, Ny):
+    """
+    create a phase amplitude pattern
+    """
+
+
+
+def fraunhoffer_diffraction():
+
+    # surface coordinates before propagation
+    ds = 0.4e-3
+    sigma, eta = spatial_grid(x_max=1e-2, dx=ds, y_max=1e-2, dy=ds)
+    sigma = sigma.reshape(1, -1, 1, 1)
+    eta = eta.reshape(-1, 1, 1, 1)
+
+    # surface coordinates after propagation
+    x, y = spatial_grid(x_max=1e-2, dx=ds, y_max=1e-2, dy=ds)
+    x = x.reshape(1, 1, 1, -1)
+    y = y.reshape(1, 1, -1, 1)
+
+    # define initial field
+    U_initial = np.zeros_like(sigma * eta, dtype=np.complex128)
+    U_initial[:,18:20] = 1
+    U_initial[:,30:32] = 1
+
+    wavelength = 500*1e-9
+    k = 2*np.pi / wavelength
+    z = 100 # meter
+
+    U_prop = U_initial*np.exp(-1j * (2*np.pi / (wavelength*z)) * (x * sigma + y * eta))
+    # integrate over surface
+    U_prop = np.sum(U_prop, axis=0)*ds
+    U_prop = np.sum(U_prop, axis=0)*ds
+
+    plt.figure()
+    plt.pcolormesh(np.squeeze(np.abs(U_initial)))
+    plt.figure()
+    plt.pcolormesh(np.squeeze(np.abs(U_prop)))
+    plt.show()
+
+
+
+
 if __name__ == "__main__":
 
-    fresnel_diffraction()
+    # fresnel_diffraction()
+    fraunhoffer_diffraction()
 
