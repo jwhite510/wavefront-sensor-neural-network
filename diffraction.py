40c6d13776688dabcb53bffd7c2faf5acad67b59
jonathon <jonathonwhite@protonmai.com>
2019-11-27 13:52:26 +0100

    class for diffracted object

--- diffraction.py
+++ diffraction.py
@@ -2,6 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+class DiffractionObject():
+    def __init__(self, ds, y_max, x_max, wavelength):
+        # surface coordinates before propagation
+        self.ds = ds
+        self.y_max = y_max
+        self.wavelength = wavelength
+        self.x_max = x_max
+        # self.ds = 0.4e-3
+        self.sigma, self.eta = spatial_grid(x_max=self.x_max, dx=self.ds, y_max=self.y_max, dy=self.ds)
+        self.sigma = self.sigma.reshape(1, -1, 1, 1)
+        self.eta = self.eta.reshape(-1, 1, 1, 1)
+
+        # surface coordinates after propagation
+        self.x, self.y = spatial_grid(x_max=self.x_max, dx=self.ds, y_max=self.y_max, dy=self.ds)
+        self.x = self.x.reshape(1, 1, 1, -1)
+        self.y = self.y.reshape(1, 1, -1, 1)
+
+        self.U_initial = None
+
+
+
 def spatial_grid(x_max, dx, y_max, dy):
 
     x_grid = np.arange(-x_max, x_max, dx)
@@ -67,35 +88,19 @@ def create_pattern(Nx, Ny):
 
 
 
-def fraunhoffer_diffraction():
+def fraunhoffer_diffraction(object, z):
 
-    # surface coordinates before propagation
-    ds = 0.4e-3
-    sigma, eta = spatial_grid(x_max=1e-2, dx=ds, y_max=1e-2, dy=ds)
-    sigma = sigma.reshape(1, -1, 1, 1)
-    eta = eta.reshape(-1, 1, 1, 1)
+    # wavelength = 500*1e-9
+    k = 2*np.pi / object.wavelength
+    # z = 100 # meter
 
-    # surface coordinates after propagation
-    x, y = spatial_grid(x_max=1e-2, dx=ds, y_max=1e-2, dy=ds)
-    x = x.reshape(1, 1, 1, -1)
-    y = y.reshape(1, 1, -1, 1)
-
-    # define initial field
-    U_initial = np.zeros_like(sigma * eta, dtype=np.complex128)
-    U_initial[:,18:20] = 1
-    U_initial[:,30:32] = 1
-
-    wavelength = 500*1e-9
-    k = 2*np.pi / wavelength
-    z = 100 # meter
-
-    U_prop = U_initial*np.exp(-1j * (2*np.pi / (wavelength*z)) * (x * sigma + y * eta))
+    U_prop = object.U_initial*np.exp(-1j * (2*np.pi / (object.wavelength*z)) * (object.x * object.sigma + object.y * object.eta))
     # integrate over surface
-    U_prop = np.sum(U_prop, axis=0)*ds
-    U_prop = np.sum(U_prop, axis=0)*ds
+    U_prop = np.sum(U_prop, axis=0)*object.ds
+    U_prop = np.sum(U_prop, axis=0)*object.ds
 
     plt.figure()
-    plt.pcolormesh(np.squeeze(np.abs(U_initial)))
+    plt.pcolormesh(np.squeeze(np.abs(object.U_initial)))
     plt.figure()
     plt.pcolormesh(np.squeeze(np.abs(U_prop)))
     plt.show()
@@ -106,5 +111,11 @@ def fraunhoffer_diffraction():
 if __name__ == "__main__":
 
     # fresnel_diffraction()
-    fraunhoffer_diffraction()
+    object = DiffractionObject(ds=0.4e-3, y_max=1e-2, x_max=1e-2, wavelength=500e-9)
+    # define initial field
+    object.U_initial = np.zeros_like(object.sigma * object.eta, dtype=np.complex128)
+    object.U_initial[:,18:20] = 1
+    object.U_initial[:,30:32] = 1
+
+    fraunhoffer_diffraction(object, z=100)
 
