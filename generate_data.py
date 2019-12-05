3c72639f8549c57c9546e85d14984757f05f61ac
jonathon <jonathonwhite@protonmai.com>
2019-12-05 16:01:34 +0100

    change name of diffraction.py, and move some definitions above if name main, new file for generating the data set

---
+++ generate_data.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tables
+import diffraction_functions
+
+
+def make_dataset(filename, N, samples):
+    # create the tables file
+
+    with tables.open_file(filename, "w") as hdf5file:
+
+        # create array for the object
+        hdf5file.create_earray(hdf5file.root, "object_amplitude", tables.Float64Atom(), shape=(0,N*N))
+
+        # create array for the object phase
+        hdf5file.create_earray(hdf5file.root, "object_phase", tables.Float64Atom(), shape=(0,N*N))
+
+        # create array for the image
+        hdf5file.create_earray(hdf5file.root, "diffraction", tables.Float64Atom(), shape=(0,N*N))
+
+        hdf5file.create_earray(hdf5file.root, "N", tables.Float64Atom(), shape=(0,1))
+
+        hdf5file.close()
+
+
+    with tables.open_file(filename, mode='a') as hd5file:
+
+        # save the dimmensions of the data
+        hd5file.root.N.append(np.array([[N]]))
+        for _ in range(samples):
+
+            # generate a sample
+            object, object_phase = diffraction_functions.make_object(N, min_indexes=4, max_indexes=8)
+            object_with_phase = diffraction_functions.make_object_phase(object, object_phase)
+
+            diffraction_pattern = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(object_with_phase)))
+
+            hd5file.root.object_amplitude.append(object.reshape(1,-1))
+            hd5file.root.object_phase.append(object_phase.reshape(1,-1))
+            hd5file.root.diffraction.append(np.abs(diffraction_pattern).reshape(1,-1))
+
+
+if __name__ == "__main__":
+    # generate a data set
+    N = 40
+
+    make_dataset("testabc.hdf5", N=N, samples=10)
+
+    make_dataset("testabc.hdf5", N=N, samples=10)
+
+    # test open the data set
+    index = 4
+    with tables.open_file("testabc.hdf5", mode="r") as hdf5file:
+
+        object = hdf5file.root.object_amplitude[index,:].reshape(N,N)
+        object_phase = hdf5file.root.object_phase[index,:].reshape(N,N)
+        diffraction = hdf5file.root.diffraction[index,:].reshape(N,N)
+
+        object_with_phase = diffraction_functions.make_object_phase(object, object_phase)
+        diffraction_functions.plot_fft(object_with_phase)
+        plt.figure(43546)
+        plt.pcolormesh(diffraction)
+        plt.show()
+
+
+
+
+
+
+
+
+
+
