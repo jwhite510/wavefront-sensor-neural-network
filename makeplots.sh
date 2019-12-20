70bccff3d1fe7640a6192a871a0f502636037d67
jonathon <jonathonwhite@protonmail.com>
2019-12-20 11:53:33 +0100

    run name variable

--- makeplots.sh
+++ makeplots.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-python makeplots.py ~/ara_data/tensorboard_graph/phase_and_amplitude_logloss_1_long/
+run_name=phase_and_amplitude_logloss_1_long/
+echo $run_name
+python makeplots.py ~/ara_data/tensorboard_graph/$run_name
