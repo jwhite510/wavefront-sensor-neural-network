489fa08ca14d9a877c7d22149413972f9308a5e1
jonathon <jonathonwhite@protonmail.com>
2020-02-06 16:52:07 +0100

    fixed shell script, run data generation in working directory

--- run_tests.sh
+++ run_tests.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 # generate dataset
 rm ./*.hdf5
 echo generating dataset
-~/python_compiled/bin/python3 ~/projects/diffraction_run/dataset_generators/with_phase_subtraction.py
+~/python_compiled/bin/python3 ~/projects/diffraction_run/with_phase_subtraction.py
 export batch_run_name=AUWNKL_withphasesubtraction1
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 export batch_run_name=AUWNKL_withphasesubtraction2
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 
 rm ./*.hdf5
 echo generating dataset
-~/python_compiled/bin/python3 ~/projects/diffraction_run/dataset_generators/withOUT_phase_subtraction.py
+~/python_compiled/bin/python3 ~/projects/diffraction_run/withOUT_phase_subtraction.py
 export batch_run_name=AUWNKL_withOUTphasesubtraction1
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 export batch_run_name=AUWNKL_withOUTphasesubtraction2
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 
 rm ./*.hdf5
 echo generating dataset
-~/python_compiled/bin/python3 ~/projects/diffraction_run/dataset_generators/with_phase_subtraction_increaseradius.py
+~/python_compiled/bin/python3 ~/projects/diffraction_run/with_phase_subtraction_increaseradius.py
 export batch_run_name=AUWNKL_withphasesubtraction_increaseradius1
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 export batch_run_name=AUWNKL_withphasesubtraction_increaseradius2
+echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 
