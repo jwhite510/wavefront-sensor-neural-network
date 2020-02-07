f2bfee6f1cf25f724418c9cc3908c9296689be4d
jonathon <jonathonwhite@protonmail.com>
2020-02-07 10:52:26 +0100

    copy the newly made graphs to a new folder

--- makeplots.sh
+++ makeplots.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-run_name=phase_and_amplitude_logloss_1_long/
-echo $run_name
-python makeplots.py ~/ara_data/tensorboard_graph/$run_name
+run_name=AHGNKL_withphasesubtraction_increaseradius1
+dest_dir=$HOME/Documents/AHGNKL_run/
+
+python makeplots.py ~/ara_data/runs/tensorboard_graph/$run_name/
+mkdir $dest_dir
+mkdir $dest_dir$run_name/
+cp ~/ara_data/runs/tensorboard_graph/$run_name/csv/$run_name.png $dest_dir$run_name/
