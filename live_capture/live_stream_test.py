d89b45569afdec403d437f4e0b4e0a4a075d98e3
jonathon <jonathonwhite@protonmail.com>
2020-03-06 16:52:46 +0100

    updating the colorbar while plotting streaming video

--- live_capture/live_stream_test.py
+++ live_capture/live_stream_test.py
@@ -20,11 +20,31 @@ if __name__ == "__main__":
 
     kernel = np.ones((5, 5), np.uint8)  # Create a Kernel for OpenCV erode function
 
+    fig, ax = plt.subplots(1,1, figsize=(5,5))
+    plt.ion()
+    first_image = False
+
     while lastkey != 27:
             if Tis.Snap_image(1) is True:  # Snap an image with one second timeout
                     image = Tis.Get_image()  # Get the image. It is a numpy array
+
+                    if not first_image:
+                        im = ax.imshow(image[:,:,0], vmin=0.0, vmax=20)
+                        colorbar = fig.colorbar(im, ax=ax)
+                        first_image = True
+                    else:
+                        im.set_data(image[:,:,0])
+                        print("calling set_clim")
+                        print("np.min(image) =>", np.min(image))
+                        print("np.max(image) =>", np.max(image))
+                        im.set_clim(vmin=np.min(image), vmax=np.max(image))
+
+                    # print("np.min(image) =>", np.min(image))
+                    # print("np.max(image) =>", np.max(image))
+
+                    plt.pause(0.001)
+
                     image = cv2.erode(image, kernel, iterations=5)  # Example OpenCV image processing
-                    print("np.shape(image) =>", np.shape(image))
                     cv2.imshow('Window', image)  # Display the result
 
             lastkey = cv2.waitKey(10)
