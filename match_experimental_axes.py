fixed problem with matching experimental data to measured data frequency axis, the distance to the camera was off by a factor of 10

match_experimental_axes.py
@@ -8,7 +8,7 @@ from PIL import Image
 import PIL.ImageOps
 from generate_data import plot_complex
 from scipy import signal
-import cv2
+# import cv2
 import time
 import diffraction_functions
 
@@ -21,12 +21,29 @@ def plot_image(title, image, axes, axeslabel, scalef):
     plt.xlabel(axeslabel)
     plt.ylabel(axeslabel)
     plt.title(title)
+    plt.axvline(x=0.05)
+    plt.axvline(x=-0.05)
+    plt.axhline(y=0.05)
+    plt.axhline(y=-0.05)
 
 
 if __name__ == "__main__":
 
-    N = 256
-    experimental_params, amplitude_mask = diffraction_functions.get_amplitude_mask_and_imagesize(N, int(N/2))
+    # fits_file_name = "m3_scan_0000.fits"
+    # # fits_file_name = "m3_scan_0001.fits"
+    # thing = fits.open(fits_file_name)
+    # measured_diffraction_pattern = thing[0].data[0,:,:]
+    # print("np.shape(measured_diffraction_pattern) => ",np.shape(measured_diffraction_pattern))
+    # plt.figure()
+    # plt.imshow(measured_diffraction_pattern)
+    # plt.show()
+
+    # # open fits file
+    # exit()
+
+    N = 512
+    # open the object with known dimmensions
+    experimental_params, amplitude_mask = diffraction_functions.get_amplitude_mask_and_imagesize(N, int(N/4))
 
     # print("np.shape(experimental_params['object']['x']) => ",np.shape(experimental_params['object']['x']))
     # print("np.shape(experimental_params['diffraction_plane']['f']) => ",np.shape(experimental_params['diffraction_plane']['f']))
@@ -45,13 +62,13 @@ if __name__ == "__main__":
     # # # # # # # # # # # # # # #
     # open the measured data  # #
     # # # # # # # # # # # # # # #
-    fits_file_name = "/home/jonathon/Documents/test/windowshare/1.fits"
+    fits_file_name = "m3_scan_0000.fits"
     thing = fits.open(fits_file_name)
     measured_diffraction_pattern = thing[0].data[0,:,:]
 
     experimental_params = {}
     experimental_params['pixel_size'] = 27e-6 # [meters] with 2x2 binning
-    experimental_params['z_distance'] = 33e-2 # [meters] distance from camera
+    experimental_params['z_distance'] = 33e-3 # [meters] distance from camera
     experimental_params['wavelength'] = 13.5e-9 #[meters] wavelength
     diffraction_functions.get_measured_diffraction_pattern_grid(measured_diffraction_pattern, experimental_params)
 