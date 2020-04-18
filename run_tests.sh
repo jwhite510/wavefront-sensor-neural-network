99a7674876150075d595c3eaa8cd07060eb4799a
jonathon <jonathonwhite@protonmail.com>
2020-04-18 16:25:30 +0200

    shell script for generating data

--- run_tests.sh
+++ run_tests.sh
@@ -7,11 +7,19 @@
 # echo generating dataset
 # ~/python_compiled/bin/python3 generate_data.py
 
-export batch_run_name=cdatanoNAN3
+# generate dataset
+cd zernike3/build/
+rm ./*.hdf5
+./a.out --name test.hdf5 --count 200
+./a.out --name train.hdf5 --count 200
+cd ../..
+
+
+export batch_run_name=cdatanoNAN4
 echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 
-export batch_run_name=cdatanoNAN3
+export batch_run_name=cdatanoNAN4
 echo submtting network training job $batch_run_name
 sbatch --wait submit_gpu_job.slurm # start training network
 
