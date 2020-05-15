fa392704ede7dc8da8660306c8e9cf3fb5514c19
jonathon <jonathonwhite@protonmail.com>
2020-05-15 11:22:28 -0400

    remove extra class instantiation

--- diffraction_net.py
+++ diffraction_net.py
@@ -220,9 +220,9 @@ class DiffractionNet():
                     m = getMeasuredDiffractionPattern.format_measured_diffraction_pattern(s, transform)
                     self.experimental_traces[sample_name] = m
 
-        getMeasuredDiffractionPattern = GetMeasuredDiffractionPattern(N_sim=self.get_data.N,
-                N_meas=np.shape(s1)[0], # for calculating the measured frequency axis (not really needed)
-                experimental_params=experimental_params)
+        # getMeasuredDiffractionPattern = GetMeasuredDiffractionPattern(N_sim=self.get_data.N,
+        #         N_meas=np.shape(s1)[0], # for calculating the measured frequency axis (not really needed)
+        #         experimental_params=experimental_params)
 
         # create tf loggers for experimental traces
         self.tf_loggers_experimentaltrace = {}
