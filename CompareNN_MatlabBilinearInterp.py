380f50c1f62739e8fd660e4a314b3e96e10b0ca6
jonathon <jonathonwhite@protonmail.com>
2020-06-03 22:56:01 +0200

    plots showing steps and interpolation grid nicely

--- CompareNN_MatlabBilinearInterp.py
+++ CompareNN_MatlabBilinearInterp.py
@@ -76,10 +76,10 @@ if __name__ == "__main__":
     plt.pcolormesh(amplitude_mask)
     plt.title("amplitude_mask")
 
-    # # retrieve the object from matlab CDI code
-    # matlabcdi_retrieved=diffraction_functions.matlab_cdi_retrieval(np.squeeze(nn_retrieved['measured_pattern']),amplitude_mask)
-    # with open("matlabcdi_retrieved.p","wb") as file:
-        # pickle.dump(matlabcdi_retrieved,file)
+    # retrieve the object from matlab CDI code
+    matlabcdi_retrieved=diffraction_functions.matlab_cdi_retrieval(np.squeeze(nn_retrieved['measured_pattern']),amplitude_mask)
+    with open("matlabcdi_retrieved.p","wb") as file:
+        pickle.dump(matlabcdi_retrieved,file)
     with open("matlabcdi_retrieved.p","rb") as file:
         matlabcdi_retrieved=pickle.load(file)
     diffraction_functions.plot_amplitude_phase_meas_retreival(matlabcdi_retrieved,"matlabcdi_retrieved")
@@ -87,14 +87,28 @@ if __name__ == "__main__":
     # check if t
     matlab_complex_object=matlabcdi_retrieved["real_output"]+1j*matlabcdi_retrieved["imag_output"]
 
+    plt.figure()
+    plt.title("real retrieved complex object")
+    plt.pcolormesh(np.real(matlab_complex_object))
+    plt.colorbar()
+
     z=[]
     plt.figure()
     plt.title("interpolation points")
     plt.pcolormesh(np.real(matlab_complex_object))
+    plt.colorbar()
     for _x,_y in zip(x,y):
         plt.scatter(_x,_y,s=2.0,color="red")
         z.append(matlab_complex_object[_y,_x])
     z=np.array(z)
+
+    plt.figure()
+    plt.title("interpolation grid")
+    plt.scatter(x,y,c=np.real(z))
+    # plt.scatter(x,y,c=np.real(z),cmap="jet")
+    plt.gca().set_xlim(0,128)
+    plt.gca().set_ylim(0,128)
+    plt.colorbar()
 
     finterp_real=interpolate.interp2d(x,y,np.real(z),kind='linear')
     finterp_imag=interpolate.interp2d(x,y,np.imag(z),kind='linear')
@@ -110,17 +124,22 @@ if __name__ == "__main__":
             interp_y
             )
 
-    fig,ax=plt.subplots(1,2)
-    ax[0].pcolormesh(np.real(matlab_complex_object))
-    ax[0].set_title("real")
-    ax[1].pcolormesh(np.imag(matlab_complex_object))
-    ax[1].set_title("imag")
+    plt.figure()
+    plt.title("AFTER interpolation")
+    plt.pcolormesh(interp_x,interp_y,z_interp_real)
+    plt.colorbar()
 
-    fig,ax=plt.subplots(1,2)
-    ax[0].pcolormesh(interp_x,interp_y,z_interp_real)
-    ax[0].set_title("real, INTERP")
-    ax[1].pcolormesh(interp_x,interp_y,z_interp_imag)
-    ax[1].set_title("imag, INTERP")
+    # fig,ax=plt.subplots(1,2)
+    # ax[0].pcolormesh(np.real(matlab_complex_object))
+    # ax[0].set_title("real")
+    # ax[1].pcolormesh(np.imag(matlab_complex_object))
+    # ax[1].set_title("imag")
+
+    # fig,ax=plt.subplots(1,2)
+    # ax[0].pcolormesh(interp_x,interp_y,z_interp_real)
+    # ax[0].set_title("real, INTERP")
+    # ax[1].pcolormesh(interp_x,interp_y,z_interp_imag)
+    # ax[1].set_title("imag, INTERP")
 
     plt.show()
 
