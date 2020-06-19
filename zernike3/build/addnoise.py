fixed numpy error with normalization

zernike3/build/addnoise.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 
                 diffraction_pattern_with_noise_poisson_and_camera=diffraction_pattern_with_noise_poisson+camera_noise
                 # normalize
-                diffraction_pattern_with_noise_poisson_and_camera *= (1/np.max(diffraction_pattern_with_noise_poisson_and_camera))
+                diffraction_pattern_with_noise_poisson_and_camera=diffraction_pattern_with_noise_poisson_and_camera/np.max(diffraction_pattern_with_noise_poisson_and_camera)
 
                 newhd5file.root.object_real.append(object_real.reshape(1,-1))
                 newhd5file.root.object_imag.append(object_imag.reshape(1,-1))