82c37ff84b632e2e8353c825bf9edd13fae8d8fa
jonathon <jonathonwhite@protinmail.com>
2020-08-21 13:07:59 +0000

    save all hdf5 files to include their network name
    fix filename

--- run_tests.sh
+++ run_tests.sh
@@ -5,30 +5,27 @@
 # source zernike3/loadmodules.sh
 
 declare -a runs=(
-# "newgputest"
-# "200"
-"xuvresult_1"
+"8_20_2020_xuvresult"
 "36000"
 )
 i=0
 while (( $i < ${#runs[@]}))
 do
-	run_name=${runs[$i]}
+	network=${runs[$i]}
 	training_samples=${runs[$i+1]}
 
-	echo $run_name
+	echo $network
 	echo $training_samples
 
 	# generate dataset
 	cd zernike3/build/
-	rm ./*.hdf5
-	# ./a.out --name test.hdf5 --count 200
-	mpirun -np 2 a.out --count 200 --name test.hdf5 --buffersize 100
-	# ./a.out --name train.hdf5 --count 200
-	mpirun -np 20 a.out --count $training_samples --name train.hdf5 --buffersize 100
+	rm ./${network}*.hdf5
+	# create samples
+	mpirun -np 2 a.out --count 200 --name ${network}_test.hdf5 --buffersize 100
+	mpirun -np 20 a.out --count $training_samples --name ${network}_train.hdf5 --buffersize 100
 	cd ../..
 
-	python diffraction_net.py $run_name
+	python diffraction_net.py ${network}
 
 	i=$i+2
 done
