2da544701805b6bd8bbdab3035d8af3863741a30
jonathon <jonathonwhite@protinmail.com>
2020-08-24 20:32:34 +0000

    re generate validation set so the wavefront sensor is written to dat file

--- noise_test_compareCDI.sh
+++ noise_test_compareCDI.sh
@@ -7,6 +7,13 @@ rm -rf ./error_*.p
 camera_noise="../../SquareWFtest/CameraNoise/1_1000/Bild_1.png"
 DIR="8_13_20_test"
 network="8_20_2020_center_intensity"
+
+# remake test hdf5
+cd zernike3/build/
+rm ./${network}_test.hdf5
+mpirun -np 2 a.out --count 200 --name ${network}_test.hdf5 --buffersize 100 --seed 345678
+cd ../..
+
 rm -rf ./$DIR
 cd zernike3/build/
 python addnoise.py --infile ${network}_test.hdf5 --outfile ${network}_test_noise.hdf5 --peakcount 50 --cameraimage $camera_noise
