86c307f771b3c7f828f7cb0adc7192ab054e7e0e
jonathon <jonathonwhite@protinmail.com>
2020-08-31 16:47:02 +0000

    making publication results

--- TestPropagate.py
+++ TestPropagate.py
@@ -12,6 +12,9 @@ import os
     # ax[0].pcolormesh(np.abs(complex_b))
     # ax[0].set_title("ABS")
 
+def make_nice_figure(retrieved:dict):
+    print(retrieved.keys())
+
 
 if __name__=="__main__":
     folder_dir="nn_pictures/teslatest5_doubleksize_doublefilters_reconscostfunction_pictures/46/measured/"
@@ -35,52 +38,41 @@ if __name__=="__main__":
 
     f_object=getMeasuredDiffractionPattern.simulation_axes['diffraction_plane']['f']
 
-    # x_pos = f_object * (experimental_params['wavelength'] * experimental_params['z_distance'])
-    # # frequency axis for propagation
-    # x_pos_dx=x_pos[-1]-x_pos[-2]
-    # N=f_object.shape[0]
-    # x_d_alpha=1 / (N * x_pos_dx)
-    # x_alpha_max=(x_d_alpha * N)/2
-    # x_alpha=np.arange(-x_alpha_max,x_alpha_max,x_d_alpha)
+    # distance to focus
+    z = 800e-6
+    gamma=np.sqrt(
+            1-
+            (experimental_params['wavelength']*f_object.reshape(-1,1))**2-
+            (experimental_params['wavelength']*f_object.reshape(1,-1))**2+
+            0j # complex so it can be square rooted and imaginry
+            )
+    k_sq = 2 * np.pi * z / experimental_params['wavelength']
+    # transfer function
+    H = np.exp(1j * np.real(gamma) * k_sq) * np.exp(-1 * np.imag(gamma) * k_sq)
 
-    # import ipdb; ipdb.set_trace() # BREAKPOINT
-    # print("BREAKPOINT")
+    complex_beam_f=np.fft.fftshift(np.fft.fft2(np.fft.fftshift(complex_beam)))
+    complex_beam_f*=H
+    complex_beam_prop=np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(complex_beam_f)))
 
     fig = diffraction_functions.plot_amplitude_phase_meas_retreival(
-            {"measured_pattern":np.zeros_like(np.abs(complex_beam)),
-            "tf_reconstructed_diff":np.zeros_like(np.abs(complex_beam)),
-            "real_output":np.real(complex_beam),
-            "imag_output":np.imag(complex_beam)},
-            "complex_beam")
+            {"measured_pattern":np.zeros_like(np.abs(complex_beam_prop)),
+            "tf_reconstructed_diff":np.zeros_like(np.abs(complex_beam_prop)),
+            "real_output":np.real(complex_beam_prop),
+            "imag_output":np.imag(complex_beam_prop)},
+            "z:-500 um -> + "+str(round(z*1e6,1))+" um [Simulated]",plot_spherical_aperture=True)
+    # plt.savefig(str(_i)+"_SIMULATEDpropfrom-500z__"+str(round(z*1e6,1))+"_um_prop.png")
+    plt.close(fig)
+    print("saving:"+str(z*1e6)+"um")
+    # plt.show()
 
-
-
-    # z = 0.5 # meters distance traveled
-    for _i,z in enumerate(np.arange(-500e-6,2000e-6,100e-6)):
-        gamma=np.sqrt(
-                1-
-                (experimental_params['wavelength']*f_object.reshape(-1,1))**2-
-                (experimental_params['wavelength']*f_object.reshape(1,-1))**2+
-                0j # complex so it can be square rooted and imaginry
-                )
-        k_sq = 2 * np.pi * z / experimental_params['wavelength']
-        # transfer function
-        H = np.exp(1j * np.real(gamma) * k_sq) * np.exp(-1 * np.imag(gamma) * k_sq)
-
-        complex_beam_f=np.fft.fftshift(np.fft.fft2(np.fft.fftshift(complex_beam)))
-        complex_beam_f*=H
-        complex_beam_prop=np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(complex_beam_f)))
-
-        fig = diffraction_functions.plot_amplitude_phase_meas_retreival(
-                {"measured_pattern":np.zeros_like(np.abs(complex_beam_prop)),
-                "tf_reconstructed_diff":np.zeros_like(np.abs(complex_beam_prop)),
-                "real_output":np.real(complex_beam_prop),
-                "imag_output":np.imag(complex_beam_prop)},
-                "z:-500 um -> + "+str(round(z*1e6,1))+" um [Simulated]",plot_spherical_aperture=True)
-        plt.savefig(str(_i)+"_SIMULATEDpropfrom-500z__"+str(round(z*1e6,1))+"_um_prop.png")
-        plt.close(fig)
-        print("saving:"+str(z*1e6)+"um")
-
+    # create dictionary for publication figure
+    retrieved = {}
+    retrieved['300_sim'] = {
+            "measured_pattern":np.zeros_like(np.abs(complex_beam_prop)),
+            "tf_reconstructed_diff":np.zeros_like(np.abs(complex_beam_prop)),
+            "real_output":np.real(complex_beam_prop),
+            "imag_output":np.imag(complex_beam_prop)
+            }
 
 
     # compare the z - 500 nm propagation to the 0 nm retrieval
@@ -89,8 +81,9 @@ if __name__=="__main__":
     with open(filename,"rb") as file:
         obj=pickle.load(file)
     fig=diffraction_functions.plot_amplitude_phase_meas_retreival(obj,"Retrieval at z=0 um")
-    plt.savefig("z0retrieval")
+    # plt.savefig("z0retrieval")
     plt.close(fig)
+    retrieved['0'] = obj
 
     # plot the original retrieval at -500 nm
     sample_name="Data_for_Jonathon_z-500_1-fits_ud_1-0_reconstructed.p"
@@ -98,5 +91,10 @@ if __name__=="__main__":
     with open(filename,"rb") as file:
         obj=pickle.load(file)
     fig=diffraction_functions.plot_amplitude_phase_meas_retreival(obj,"Retrieval at z=-500 um")
-    plt.savefig("z-500retrieval")
+    # plt.savefig("z-500retrieval")
     plt.close(fig)
+    retrieved['-500'] = obj
+
+    make_nice_figure(retrieved)
+
+    plt.show()
